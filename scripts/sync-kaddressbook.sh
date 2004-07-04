10390b1c1893ae9b389c74b30b9be9ce49ce5f8d
Jorg Schuler <jcsjcs@users.sourceforge.net>
2004-07-04 10:06:55 +0000

    Removed kaddressbook_ipod.sh as it is superseded by ipod_kaddresbook.sh
    Changed version to 0.80
    
    Moved id3tag detection to PKG_CHECK_MODULES() in configure.in
    
    Fixed scripts/Makefile.am to include the scripts into the distribution.
    
    git-svn-id: https://gtkpod.svn.sf.net/svnroot/gtkpod/gtkpod/trunk@408 f01d2545-417e-4e96-918e-98f8d0dbbcb6

--- scripts/sync-kaddressbook.sh
+++ scripts/sync-kaddressbook.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 # (c) 2004 Markus Gaugusch <markus@gaugusch.at>
 # Script for syncing kaddressbook data with iPod
-#
-# Changelog:
-#
-# 2004/06/27:
-# Changed to accept ipod-mountpath and encoding as command line
-# option
-#
-# Split into two files -- one for contacts, one for calendar.
-#
-# Placed under GPL in agreement with Markus Gaugusch.
-#
-# Added note about encoding used by the iPod.
-#
-# -- Jorg Schuler <jcsjcs at users dot sourceforge dot net>.
 
+# Usage:
+# 
+# sync-kaddressbook.sh [-i <ipod mountpoint>] [-d <kaddressbook data file>]
+#           ...        [-e <encoding>]
+#
+# with the following defaults: 
 
-# About the encoding used by the iPod:
+IPOD_MOUNT=/mnt/ipod                          # mountpoint of ipod
+DATAFILE=~/.kde/share/apps/kabc/std.vcf       # vcard file
+ENCODING=ISO-8859-15                          # encoding used by ipod
+
+# Unless called with "-e=none" this script requires "recode" available
+# from ftp://ftp.iro.umontreal.ca/pub/recode/recode-3.6.tar.gz
+
+# About the encoding used by the iPod (by Jorg Schuler):
 #
 # For some reason the encoding used for the contact files and
 # calenader files depends on the language you have set your iPod
@@ -35,21 +34,57 @@
 # Japanese           SHIFT-JIS
 
 
-ENCODING=ISO-8859-15                    # encoding used by ipod
-IPOD_MOUNT=/mnt/ipod                    # mountpoint of ipod
-VF=~/.kde/share/apps/kabc/std.vcf       # vcard file
+# Changelog:
+#
+# 2004/06/27 (Jorg Schuler <jcsjcs at users dot sourceforge dot net>):
+# Changed to accept ipod-mountpath and encoding as command line
+# option
+#
+# Split into two files -- one for contacts, one for calendar.
+#
+# Placed under GPL in agreement with Markus Gaugusch.
+#
+# Added note about encoding used by the iPod.
+#
+# 2004/07/02 (Jorg Schuler <jcsjcs at users dot sourceforge dot net>):
+# Added Usage-line, added check for vcard file, rearranged source
+#
+# 2004/07/03 (Jorg Schuler <jcsjcs at users dot sourceforge dot net>):
+# Made "recode" optional (call with -e="none")
+#
+# Removed "dos2unix" as my iPod (firmware 1.3) happily accepted
+# DOS-type vcards as well. Instead changed the "grep" pattern to
+# catch \cr\lf as well.
+
 
 # overwrite default settings with optional command line arguments
-while getopts i:d: option; do
+while getopts i:d:e: option; do
     case $option in
         i) IPOD_MOUNT=$OPTARG;;
-        d) VF=$OPTARG;;
+        d) DATAFILE=$OPTARG;;
         e) ENCODING=$OPTARG;;
-        \?) echo "usage: [-i <ipod mountpoint>] [-d <kaddressbook data file>] [-e <encoding>]";;
+        \?) echo "Usage: `basename $0` [-i <ipod mountpoint>] [-d <kaddressbook data file>] [-e <encoding>]"
+	    exit 1;;
     esac
 done
 
 
+# set the RECODE command
+if [ $ENCODING = "none" ] || [ $ENCODING = "NONE" ]; then
+    RECODE="cat"    # no conversion
+else
+    RECODE="recode UTF8..$ENCODING"
+fi
+
+
+# check if DATAFILE exists
+if [ ! -f $DATAFILE ]; then
+    echo "Error: $DATAFILE does not exist"
+    exit 1
+fi
+
+
+# remove all empty lines and recode if necessary
 echo -n "Syncing iPod ... [Contacts] "
-cat $VF |dos2unix | grep -v '^$' | recode UTF8..$ENCODING > $IPOD_MOUNT/Contacts/`basename $VF`
+cat $DATAFILE | grep -v '^[[:space:]]$\|^$' | $RECODE > $IPOD_MOUNT/Contacts/`basename $DATAFILE`
 echo "done!"
