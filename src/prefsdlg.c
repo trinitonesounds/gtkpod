2fb3eb6a956b53cf7b14cc792bda1886eab96a08
Matvey Kozhev <sikon@users.sourceforge.net>
2007-12-21 18:41:49 +0000

    Implemented the first tab of the preferences dialog
    git-svn-id: https://gtkpod.svn.sf.net/svnroot/gtkpod/gtkpod/trunk@1857 f01d2545-417e-4e96-918e-98f8d0dbbcb6

--- src/prefsdlg.c
+++ src/prefsdlg.c
@@ -32,29 +32,370 @@
 #include <gtk/gtk.h>
 #include "misc.h"
 #include "help.h"
+#include "prefs.h"
 
+/*
+	Begin types
+*/
+typedef struct _ind_string
+{
+	gint index;
+	const gchar *string;
+} ind_string;
+
+ind_string toolbar_styles[] = {
+	{ -1, N_("Hide") },
+	{ GTK_TOOLBAR_ICONS, N_("Icons only") },
+	{ GTK_TOOLBAR_TEXT, N_("Text only") },
+	{ GTK_TOOLBAR_BOTH, N_("Text under icons") },
+	{ GTK_TOOLBAR_BOTH_HORIZ, N_("Text beside icons") },
+	{ -1, NULL }
+};
+
+#define IND_STRING_END(x, i) ((x)[i].string == NULL)
+#define COUNTOF(x) (sizeof(x) / sizeof((x)[0]))
+
+/*
+	Begin data
+*/
+const gchar *checkbox_map[][2] = {
+	/* "checkbox_id", "preference" */
+	{ "group_compilations", "group_compilations" },
+	{ "include_neverplayed", "not_played_track" },
+	{ "add_subfolders", "add_recursively" },
+	{ "allow_duplicates", "!sha1" },
+	{ "delete_missing", "sync_delete_tracks" },
+};
+
+static GladeXML *prefs_xml = NULL;
+static GtkWidget *prefs_dialog = NULL;
+
+/*
+	Convenience functions
+*/
+void combo_box_clear (GtkComboBox *combo)
+{
+	GtkListStore *store;
+	GtkTreeModel *model = gtk_combo_box_get_model (combo);
+	g_return_if_fail (GTK_IS_LIST_STORE (model));
+	store = GTK_LIST_STORE (model);
+	gtk_list_store_clear (store);
+}
+
+gint ind_string_find(ind_string *array, gint index)
+{
+	gint i;
+	
+	for(i = 0; !IND_STRING_END(array, i); i++)
+	{
+		if(array[i].index == index)
+			return i;
+	}
+	
+	return -1;
+}
+
+void ind_string_fill_combo (ind_string *array, GtkComboBox *combo)
+{
+	gint i;
+	combo_box_clear(combo);
+
+	for (i = 0; !IND_STRING_END(array, i); i++)
+	{
+		gtk_combo_box_append_text(combo, gettext(array[i].string));
+	}
+}
+
+void init_checkbox (GtkToggleButton *checkbox, const gchar *pref)
+{
+	g_object_set_data(G_OBJECT(checkbox), "pref", (gchar *) pref);
+	
+	if(pref[0] == '!')		/* Checkbox is !preference */
+		gtk_toggle_button_set_active(checkbox, !prefs_get_int(pref + 1));
+	else
+		gtk_toggle_button_set_active(checkbox, prefs_get_int(pref));
+}
+
+static gint column_tree_sort (GtkTreeModel *model,
+							  GtkTreeIter *a,
+							  GtkTreeIter *b,
+							  gpointer user_data)
+{
+    gchar *str1, *str2;
+    gint result;
+
+    gtk_tree_model_get (model, a, 0, &str1, -1);
+    gtk_tree_model_get (model, b, 0, &str2, -1);
+    result = g_utf8_collate (str1, str2);
+
+    g_free (str1);
+    g_free (str2);
+    return result;
+}
+
+static void setup_column_tree (GtkTreeView *treeview, gboolean list_visible)
+{
+    GtkListStore *store;
+    GtkTreeIter iter;
+    GtkTreeViewColumn *column;
+    GtkCellRenderer *renderer;
+    gint i;
+
+    store = gtk_list_store_new (2, G_TYPE_STRING, G_TYPE_INT);
+    column = gtk_tree_view_column_new ();
+    renderer = gtk_cell_renderer_text_new ();
+
+	gtk_tree_view_column_pack_start (column, renderer, TRUE);
+    gtk_tree_view_column_set_attributes (column, renderer, "text", 0, NULL);
+
+    gtk_tree_view_append_column (treeview, column);
+    gtk_tree_view_set_model (treeview, GTK_TREE_MODEL (store));
+
+    g_object_unref (G_OBJECT (store));
+
+    for (i = 0; i < TM_NUM_COLUMNS; i++)
+    {
+		gint visible = prefs_get_int_index("col_visible", i);
+		
+		if ((!list_visible && visible) || (list_visible && !visible))
+			continue;
+		
+		gtk_list_store_append (store, &iter);
+		gtk_list_store_set(store, &iter,
+				   0, gettext (get_tm_string (i)),
+				   1, i, -1);
+    }
+	
+	if(!list_visible)
+	{
+		/* Sort invisible columns */
+		gtk_tree_sortable_set_default_sort_func (GTK_TREE_SORTABLE (store),
+							 column_tree_sort,
+							 NULL,
+							 NULL);
+		
+		gtk_tree_sortable_set_sort_column_id (GTK_TREE_SORTABLE (store),
+						  GTK_TREE_SORTABLE_DEFAULT_SORT_COLUMN_ID,
+						  GTK_SORT_ASCENDING);
+	}
+}
+
+GtkTreeIter tree_get_current_iter (GtkTreeView *view)
+{
+	GtkTreeModel *model = gtk_tree_view_get_model (view);
+	GtkTreePath *path;
+	GtkTreeIter iter;
+
+	gtk_tree_view_get_cursor (view, &path, NULL);
+	gtk_tree_model_get_iter (model, &iter, path);
+	gtk_tree_path_free (path);
+	
+	return iter;
+}
+
+/*
+	Real functions
+*/
 void setup_prefs_dlg (GladeXML *xml, GtkWidget *dlg)
 {
+	gint i;
 	GtkWidget *toolbar_style_combo = gtkpod_xml_get_widget (xml, "toolbar_style");
-	GtkWidget *target_format_combo = gtkpod_xml_get_widget (xml, "target_format");
-
-	gtk_combo_box_set_active (GTK_COMBO_BOX(toolbar_style_combo), 0);
-	gtk_combo_box_set_active (GTK_COMBO_BOX(target_format_combo), 0);
+	
+	/* Display */
+	
+	/* Toolbar */
+	ind_string_fill_combo(toolbar_styles, GTK_COMBO_BOX (toolbar_style_combo));
+	
+	if (!prefs_get_int("display_toolbar"))
+	{
+		gtk_combo_box_set_active(GTK_COMBO_BOX (toolbar_style_combo),
+								 ind_string_find(toolbar_styles, -1));
+	}
+	else
+	{
+		gint style = prefs_get_int("toolbar_style");
+		gint index = ind_string_find(toolbar_styles, style);
+		
+		gtk_combo_box_set_active(GTK_COMBO_BOX (toolbar_style_combo), index);
+	}
+	/* End toolbar */
+	
+	/* Columns */
+	setup_column_tree (GTK_TREE_VIEW (gtkpod_xml_get_widget (xml, "displayed_columns")), TRUE);
+	
+	gtk_spin_button_set_value (GTK_SPIN_BUTTON (gtkpod_xml_get_widget (xml, "filter_tabs_count")),
+							   prefs_get_int("sort_tab_num"));
+	
+	/* Check boxes */
+	for (i = 0; i < COUNTOF(checkbox_map); i++)
+	{
+		init_checkbox (GTK_TOGGLE_BUTTON (gtkpod_xml_get_widget (xml, checkbox_map[i][0])),
+					   checkbox_map[i][1]);
+	}
 }
 
 void open_prefs_dlg ()
 {
-	GladeXML *xml = glade_xml_new (xml_file, "prefs_dialog", NULL);
-	glade_xml_signal_autoconnect (xml);
-	GtkWidget *dlg = gtkpod_xml_get_widget (xml, "prefs_dialog");
+	if(prefs_dialog)
+	{
+		gtk_window_present(GTK_WINDOW(prefs_dialog));
+		return;
+	}
 	
-	setup_prefs_dlg(xml, dlg);
+	prefs_xml = glade_xml_new (xml_file, "prefs_dialog", NULL);
+	prefs_dialog = gtkpod_xml_get_widget (prefs_xml, "prefs_dialog");
 	
-	gtk_dialog_run(GTK_DIALOG(dlg));
-	gtk_widget_destroy(dlg);
+	setup_prefs_dlg(prefs_xml, prefs_dialog);
+	/*
+		This is after setup because we don't want on_changed signals to fire
+		during setup
+	*/
+	glade_xml_signal_autoconnect (prefs_xml);
+	gtk_widget_show(prefs_dialog);
 }
 
+/*
+	glade callback
+*/
 void on_prefs_dialog_help ()
 {
 	gtkpod_open_help_context ("gtkpod");
 }
+
+/*
+	glade callback
+*/
+void on_prefs_dialog_close ()
+{
+	gtk_widget_destroy(prefs_dialog);
+	g_object_unref(prefs_xml);
+	
+	prefs_dialog = NULL;
+	prefs_xml = NULL;
+}
+
+/*
+	glade callback
+*/
+void on_filter_tabs_count_value_changed (GtkSpinButton *sender, gpointer e)
+{
+    gint num = gtk_spin_button_get_value_as_int (sender);
+
+	/* Update the number of filter tabs */
+    prefs_set_int ("sort_tab_num", num);
+	st_show_visible();
+}
+
+/*
+	glade callback
+*/
+void on_toolbar_style_changed (GtkComboBox *sender, gpointer e)
+{
+	gint index = toolbar_styles[gtk_combo_box_get_active(sender)].index;
+	
+	if (index == -1)
+	{
+		prefs_set_int ("display_toolbar", 0);
+	}
+	else
+	{
+		prefs_set_int ("display_toolbar", 1);
+		prefs_set_int ("toolbar_style", index);
+	}
+
+	display_show_hide_toolbar ();
+}
+
+/*
+	generic glade callback, used by many checkboxes
+*/
+void on_simple_checkbox_toggled (GtkToggleButton *sender, gpointer e)
+{
+	gboolean active = gtk_toggle_button_get_active (sender);
+	gchar *pref = (gchar *) g_object_get_data (G_OBJECT(sender), "pref");
+	
+	if(pref[0] == '!')		/* Checkbox is !preference */
+		prefs_set_int(pref + 1, !active);
+	else
+		prefs_set_int(pref, active);
+}
+
+/*
+	glade callback
+*/
+void on_group_compilations_toggled (GtkToggleButton *sender, gpointer e)
+{
+	gboolean active = gtk_toggle_button_get_active (sender);
+	
+	prefs_set_int ("group_compilations", active);
+	st_show_visible();
+}
+
+/*
+	glade callback
+*/
+void on_column_add_clicked (GtkButton *sender, gpointer e)
+{
+	gint i;
+	GladeXML *xml = glade_xml_new (xml_file, "prefs_columns_dialog", NULL);
+	GtkWidget *dlg = gtkpod_xml_get_widget (xml, "prefs_columns_dialog");
+	GtkTreeView *view = GTK_TREE_VIEW (gtkpod_xml_get_widget (xml, "available_columns"));
+	GtkTreeModel *model;
+	GtkTreeIter iter;
+	
+	setup_column_tree (view, FALSE);
+	
+	/* User pressed Cancel */
+	if(!gtk_dialog_run (GTK_DIALOG (dlg)))
+	{
+		gtk_widget_destroy (dlg);
+		g_object_unref (xml);
+		return;
+	}
+	
+	/* User pressed Add */
+	model = gtk_tree_view_get_model (view);
+	iter = tree_get_current_iter (view);
+	gtk_tree_model_get (model, &iter, 1, &i, -1);
+	
+	gtk_widget_destroy (dlg);
+	g_object_unref (xml);
+	
+	view = GTK_TREE_VIEW (gtkpod_xml_get_widget (prefs_xml, "displayed_columns"));
+	model = gtk_tree_view_get_model (view);
+	
+	gtk_list_store_append (GTK_LIST_STORE (model), &iter);
+	gtk_list_store_set(GTK_LIST_STORE (model), &iter,
+			   0, gettext (get_tm_string (i)),
+			   1, i, -1);
+	
+	prefs_set_int_index ("col_visible", i, TRUE);
+	tm_store_col_order ();
+	tm_show_preferred_columns ();
+}
+
+/*
+	glade callback
+*/
+void on_column_remove_clicked (GtkButton *sender, gpointer e)
+{
+	gint i;
+	GtkTreeView *view = GTK_TREE_VIEW (gtkpod_xml_get_widget (prefs_xml, "displayed_columns"));
+	GtkTreeModel *model = gtk_tree_view_get_model (view);
+	GtkTreeIter iter = tree_get_current_iter (view);
+	
+	if(!gtk_list_store_iter_is_valid (GTK_LIST_STORE (model), &iter))
+		return;
+
+	gtk_tree_model_get (model, &iter, 1, &i, -1);
+	gtk_list_store_remove (GTK_LIST_STORE (model), &iter);
+	
+	prefs_set_int_index ("col_visible", i, FALSE);
+	tm_store_col_order ();
+	tm_show_preferred_columns ();
+}
+
+/*
+	ADD COLUMN DIALOG
+*/
+
