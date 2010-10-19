dde851c9900333c8aaf0f4bde06a439ef8a6ed98
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2010-10-19 19:59:55 +0100

    Remove deprecated gtk_menu_append function
    * Replaced with gtk_menu_shell_append

--- libgtkpod/context_menus.c
+++ libgtkpod/context_menus.c
@@ -90,7 +90,7 @@ GtkWidget *add_sub_menu(GtkWidget *menu, gchar* label, gchar* stockid) {
     submenu = gtk_menu_new();
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(item), submenu);
 
-    gtk_menu_append(menu, item);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menu), item);
     gtk_widget_show_all(item);
 
     return submenu;
