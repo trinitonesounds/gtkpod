53c5e2fed93180407bc8368786da29c6c8e823a5
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2011-05-01 16:44:27 +0100

    Update release information
    * Move from 2.0.0 to 2.0.1

--- version.sh
+++ version.sh
@@ -4,10 +4,10 @@ if [ -d .git ]; then
   COMMIT=`git rev-parse --short HEAD`
 
   # Use this line for unstable dev builds
-  REVISION="2.0.0.${COMMIT}"
+  REVISION="2.0.1.${COMMIT}"
 
   # Use this line for releases
-  #REVISION="2.0.0"
+  #REVISION="2.0.1"
 
   echo $REVISION > version
 fi
