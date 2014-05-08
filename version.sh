46df65b8b3f2c07db248f363560f3e092bd3e362
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2014-05-08 21:55:57 +0100

    Update version script to 2.1.5

--- version.sh
+++ version.sh
@@ -4,10 +4,10 @@ if [ -d .git ]; then
   COMMIT=`git rev-parse --short HEAD`
 
   # Use this line for unstable dev builds
-  REVISION="2.1.4~${COMMIT}"
+  REVISION="2.1.5~${COMMIT}"
 
   # Use this line for releases
-  #REVISION="2.1.4"
+  #REVISION="2.1.5"
 
   echo $REVISION > version
 fi
